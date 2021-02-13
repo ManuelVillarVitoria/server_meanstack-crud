32e61748b6ca3609e08bf256ddd09872f03ff56a
Manu <manu.villar.vitoria@gmail.com>
2021-02-13 19:40:55 +0100

    production

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require("cors");
 
+
 const app = express();
 
 conectarDB();
+
+//app.set ('port', process.env.PORT || 4000)
+const port = process.env.PORT || 4000
 
 app.use(cors())
 
@@ -13,6 +17,6 @@ app.use(express.json());
 app.use('/api/productos', require('./routes/producto'));
 
 
-app.listen(4000, () => {
-    console.log('El servidor está corriendo perfectamente')
+app.listen( port, '0.0.0.0', () => { //app.get('port')
+    console.log(`El servidor está corriendo perfectamente en el puerto ${port}`)
 })
